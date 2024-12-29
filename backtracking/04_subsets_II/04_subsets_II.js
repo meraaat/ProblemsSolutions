3c8131559521353ceb03c4618f09b96929c0a643
Mohammadali Akbari <mera1takbari@gmail.com>
2024-12-29 03:01:19 +0100

    feat(backtracking/04_subsets_II): The problem and solutions are added

--- backtracking/04_subsets_II/04_subsets_II.js
+++ backtracking/04_subsets_II/04_subsets_II.js
@@ -1,3 +1,55 @@
+const testCases = [
+  {
+    input: [1, 2, 2],
+    expected: [[], [1], [2], [1, 2], [2, 2], [1, 2, 2]],
+  },
+  {
+    input: [0],
+    expected: [[], [0]],
+  },
+  {
+    input: [],
+    expected: [[]],
+  },
+];
 
-console.log('Hello from JavaScript!');
+(function () {
+  testCases.forEach((testCase, i) => {
+    console.log(`Test Case ${i + 1}:`);
+    console.log(`Input: ${JSON.stringify(testCase.input)}`);
+    const result = subsetsWithDup(testCase.input);
+    console.log(`Result: ${JSON.stringify(result)}`); // Use JSON.stringify for nested arrays
+    console.log(`Expected: ${JSON.stringify(testCase.expected)}`);
+    if (JSON.stringify(result) === JSON.stringify(testCase.expected)) {
+      console.log(`Test case ${i + 1} passed`);
+    } else {
+      console.log(`Test case ${i + 1} failed`);
+    }
+    console.log();
+  });
+})();
 
+function subsetsWithDup(nums) {
+  let result = [];
+  nums.sort((a, b) => a - b);
+
+  function helper(i, subsets) {
+    // Base case
+    if (i === nums.length) {
+      result.push([...subsets]);
+      return;
+    }
+
+    // Recursive case
+    // Include
+    subsets.push(nums[i]);
+    helper(i + 1, subsets);
+    subsets.pop(); // Backtracking
+
+    while (i < nums.length - 1 && nums[i] == nums[i + 1]) i++;
+    helper(i + 1, subsets);
+  }
+
+  helper(0, []);
+  return result;
+}
--- backtracking/04_subsets_II/go/04_subsets_II.go
+++ backtracking/04_subsets_II/go/04_subsets_II.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-    fmt.Println("Hello from Golang!")
+	testCases := []struct {
+		input []int
+	}{
+		{
+			input: []int{1, 2, 2},
+		},
+		{
+			input: []int{0},
+		},
+		{
+			input: []int{},
+		},
+	}
+
+	for i, testCase := range testCases {
+		fmt.Printf("Test Case %d:\n", i+1)
+		fmt.Printf("Input: %v\n", testCase.input)
+		inputCopy := make([]int, len(testCase.input))
+		copy(inputCopy, testCase.input)
+		result := subsetsWithDup(inputCopy)
+		fmt.Printf("Result: %v\n", result)
+
+		fmt.Println()
+	}
+}
+
+func subsetsWithDup(nums []int) [][]int {
+	result := [][]int{}
+
+	var helper func(i int, subsets []int)
+
+	helper = func(i int, subsets []int) {
+		// Base case
+		if i == len(nums) {
+			subsetsCopy := make([]int, len(subsets))
+			copy(subsetsCopy, subsets)
+			result = append(result, subsetsCopy)
+			return
+		}
+		// Recursive case
+		// Include
+		helper(i+1, append(subsets, nums[i]))
+
+		// Exclude
+		for i < len(nums)-1 && nums[i] == nums[i+1] {
+			i++
+		}
+		helper(i+1, subsets)
+
+	}
+	helper(0, []int{})
+	return result
 }
