19c7923d2097bd180feae700eb993f82fff46623
Mohammadali Akbari <mera1takbari@gmail.com>
2025-01-06 19:10:57 +0100

    feat(dynamic_programming/06): more human readable solutions added.

--- dynamic_programming/06_Edit_Distance/go/06_Edit_Distance.go
+++ dynamic_programming/06_Edit_Distance/go/06_Edit_Distance.go
@@ -5,6 +5,7 @@ import (
 	"math"
 )
 
+// Recursive
 func minDistanceRecursive(word1 string, word2 string) int {
 	var minDistance func(i, j int) int
 	minDistance = func(i, j int) int {
@@ -22,6 +23,43 @@ func minDistanceRecursive(word1 string, word2 string) int {
 	return minDistance(len(word1)-1, len(word2)-1)
 }
 
+func minDistanceRecursive2(word1, word2 string) int {
+
+	n := len(word1)
+	m := len(word2)
+
+	var numberOfOperations func(int, int) int
+	numberOfOperations = func(index1, index2 int) int {
+		// Base case
+
+		if index1 > n-1 && index2 > m-1 {
+			return 0
+		}
+
+		if index1 > n-1 {
+			return m - index2
+		}
+
+		if index2 > m-1 {
+			return n - index1
+		}
+
+		// Recursive case
+		if word1[index1] == word2[index2] {
+			return numberOfOperations(index1+1, index2+1)
+		}
+
+		insertOP := 1 + numberOfOperations(index1, index2+1)
+		deleteOP := 1 + numberOfOperations(index1+1, index2)
+		replaceOP := 1 + numberOfOperations(index1+1, index2+1)
+
+		return min(insertOP, deleteOP, replaceOP)
+	}
+
+	return numberOfOperations(0, 0)
+}
+
+// Memoization
 func minDistanceMemo(word1 string, word2 string) int {
 	memo := make([][]int, len(word1)+1)
 	for i := range memo {
@@ -53,6 +91,55 @@ func minDistanceMemo(word1 string, word2 string) int {
 
 }
 
+func minDistanceMemo2(word1, word2 string) int {
+
+	n := len(word1)
+	m := len(word2)
+
+	memo := make([][]int, n)
+	for i := range memo {
+		memo[i] = make([]int, m)
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+
+	var numberOfOperations func(int, int) int
+	numberOfOperations = func(index1, index2 int) int {
+		// Base case
+		if index1 > n-1 && index2 > m-1 {
+			return 0
+		}
+
+		if index1 > n-1 {
+			return m - index2
+		}
+
+		if index2 > m-1 {
+			return n - index1
+		}
+
+		// Recursive case
+		if memo[index1][index2] != -1 {
+			return memo[index1][index2]
+		}
+
+		if word1[index1] == word2[index2] {
+			memo[index1][index2] = numberOfOperations(index1+1, index2+1)
+		} else {
+			insertOP := 1 + numberOfOperations(index1, index2+1)
+			deleteOP := 1 + numberOfOperations(index1+1, index2)
+			replaceOP := 1 + numberOfOperations(index1+1, index2+1)
+			memo[index1][index2] = min(insertOP, deleteOP, replaceOP)
+		}
+
+		return memo[index1][index2]
+	}
+
+	return numberOfOperations(0, 0)
+}
+
+// Tabulation
 func minDistanceTab(word1 string, word2 string) int {
 	m := len(word1)
 	n := len(word2)
@@ -80,6 +167,7 @@ func minDistanceTab(word1 string, word2 string) int {
 	return dp[m][n]
 }
 
+// Space Optimized
 func minDistanceSpace(word1 string, word2 string) int {
 	m := len(word1)
 	n := len(word2)
@@ -102,12 +190,29 @@ func minDistanceSpace(word1 string, word2 string) int {
 			if word1[i-1] == word2[j-1] {
 				curr[j] = prev[j-1]
 			} else {
-				curr[j] = 1 + int(math.Min(math.Min(float64(prev[j]), float64(curr[j-1])), float64(prev[j-1])))
+				curr[j] = 1 + min(prev[j], curr[j-1], prev[j-1])
 			}
 		}
 		prev, curr = curr, prev
 	}
 	return prev[n]
+}
+
+func min[T ~int | ~float64](nums ...T) T {
+
+	if len(nums) == 0 {
+		var zero T
+		return zero
+	}
+
+	minimum := nums[0]
+	for i := range nums {
+		if nums[i] <= minimum {
+			minimum = nums[i]
+		}
+	}
+
+	return minimum
 }
 
 func main() {
@@ -121,6 +226,7 @@ func main() {
 		{"", "", 0},
 		{"a", "", 1},
 		{"", "b", 1},
+		{"abc", "ac", 1},
 		{"kitten", "sitting", 3},
 		{"apple", "aple", 1},
 		{"intention", "intention", 0},
@@ -128,7 +234,9 @@ func main() {
 
 	minDistanceFuncs := map[string]func(string, string) int{
 		"Recursive":      minDistanceRecursive,
+		"Recursive2":     minDistanceRecursive2,
 		"Memoization":    minDistanceMemo,
+		"Memoization2":   minDistanceMemo2,
 		"Tabulation":     minDistanceTab,
 		"SpaceOptimized": minDistanceSpace,
 	}
